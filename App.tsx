ffe99be78fbbf81aeb6ca4fa1699cb77f2231461
iamidlek <iamidlek@naver.com>
2022-01-01 22:53:57 +0900

    todo 만들기 시작

--- App.tsx
+++ App.tsx
@@ -12,9 +12,23 @@ import { theme } from "./colors";
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload: string) => setText(payload);
+
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    // deep copy
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+    setToDos(newToDos);
+    setText("");
+  };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -38,8 +52,10 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <TextInput
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
         value={text}
+        returnKeyType="done"
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
         style={styles.input}
       />
